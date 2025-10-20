a812160af38be6b8ee2fd7ef1e37acb20a759a4d
Kauê Rosa <Kaue.dev07@gmail.com>
2025-10-20 09:43:44 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const {somar, subtrair, multiplicar, dividir} = require('./funcoes');
+
+console.log("Soma: ", somar(10, 5));          
+console.log("Subtração: ", subtrair(10, 5));
+console.log("Multiplicação: ", multiplicar(10, 5));
+console.log("Divisão: ", dividir(10, 5));
